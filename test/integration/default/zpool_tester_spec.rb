bf23d7e7810d3ced7b57ed3f64f67851618dee52
Tim Smith <tsmith@chef.io>
2017-11-10 22:38:46 -0800

    Work around path on Solaris in the tests
    Chef does magic to find the binary in the path. Inspec only uses the PATH which does not include /usr/sbin
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/default/zpool_tester_spec.rb
+++ test/integration/default/zpool_tester_spec.rb
@@ -1,24 +1,30 @@
-describe command('zpool list') do
+cmd = if os[:family] == 'solaris'
+        '/usr/sbin/zpool'
+      else
+        'zpool'
+      end
+
+describe command("#{cmd} list") do
   its(:stdout) { is_expected.to match(/zpool1/) }
 end
 
-describe command('zpool list') do
+describe command("#{cmd} list") do
   its(:stdout) { is_expected.to match(/multiplevolumes/) }
 end
 
-describe command('zpool status multiplevolumes') do
+describe command("#{cmd} status multiplevolumes") do
   its(:stdout) { is_expected.to match(/disk2/) }
   its(:stdout) { is_expected.to match(/disk3/) }
 end
 
-describe command('zpool list') do
+describe command("#{cmd} list") do
   its(:stdout) { is_expected.not_to match(/zpool2/) }
 end
 
-describe command('zpool list') do
+describe command("#{cmd} list") do
   its(:stdout) { is_expected.to match(/test/) }
 end
 
-describe command('zpool status -v tank') do
+describe command("#{cmd} status -v tank") do
   its(:stdout) { is_expected.to match(/raidz1/) }
 end
