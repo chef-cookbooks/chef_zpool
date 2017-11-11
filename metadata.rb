bd327bb51578e1dbc4a50c1c72d0296f3882f2f5
Tim Smith <tsmith@chef.io>
2017-11-10 20:57:39 -0800

    Fix the issues URL
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description       'Manages ZFS zpools'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.1.0'
 source_url        'https://github.com/chef-cookbooks/chef_zpool'
-issues_url        'https://github.com/marthag8/zpool/issues'
+issues_url        'https://github.com/chef-cookbooks/chef_zpool/issues'
 
 %w(solaris2 ubuntu freebsd).each do |os|
   supports os
