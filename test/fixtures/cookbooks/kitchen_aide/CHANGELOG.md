9df89c2d553361825890f71e0518e1b2c29484a7
Tim Smith <tsmith@chef.io>
2017-11-10 21:43:45 -0800

    Switch to inspec and kill kitchen_aide cookbook
    There's no need for this busser hackery when you use inspec
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/kitchen_aide/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-# Revision History for kitchen_aide
-
-## 0.1.0 Initial version.
-
---
+++ test/integration/default/zpool_tester_spec.rb
@@ -0,0 +1,24 @@
+describe command('zpool list') do
+  its(:stdout) { is_expected.to match(/zpool1/) }
+end
+
+describe command('zpool list') do
+  its(:stdout) { is_expected.to match(/multiplevolumes/) }
+end
+
+describe command('zpool status multiplevolumes') do
+  its(:stdout) { is_expected.to match(/disk2/) }
+  its(:stdout) { is_expected.to match(/disk3/) }
+end
+
+describe command('zpool list') do
+  its(:stdout) { is_expected.not_to match(/zpool2/) }
+end
+
+describe command('zpool list') do
+  its(:stdout) { is_expected.to match(/test/) }
+end
+
+describe command('zpool status -v tank') do
+  its(:stdout) { is_expected.to match(/raidz1/) }
+end
